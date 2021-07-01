a6b41e56e2571b8b10e27b6f00eb3453d454af8f
Shamim630 <shamimkhan8161@gmail.com>
2021-07-01 16:25:19 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let decimalPoint=enter=entered=operatorSign=rootNpower_Sign=flo=math=M=firstI=first=second=second1=answer=theanswer=result=peSign
+let opsChck = dotCounter=0
+let removeFirstZero=""
+
+function dot(b){
+    if(decimalPoint==""){
+        enter=entered=b;
+        first+=enter;
+        entered+=enter;
+        
+    }
+}
